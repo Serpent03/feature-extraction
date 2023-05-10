3cb51a1c3d5ddf579c8cbd4984e3744b5cb7a00a
Serpent03 <dumbpilot03@gmail.com>
2023-05-10 12:43:40 +0530

    minor changes

--- brute_force.py
+++ brute_force.py
@@ -4,12 +4,15 @@ from utils import *
 
 img1 = readIm('./Images/2.jpg')
 img2 = readIm('./Images/3.jpg')
+camMtx = retCamMtx("./cameraMatrix.pkl")
+distCoeff = retDistCoeff("./dist.pkl")
 
 kp1, des1, kp2, des2 = ORB_detector(img1, img2)
-1
 numMatches = bruteForceMatcher(des1, des2)[:20]
+# numMatches = bruteForceMatcherkNN(des1, des2)
+# TODO print(numMatches)
 
-# print(numMatches[:15])
+e, _ = retEssentialMat(kp1, kp2, camMtx, distCoeff)
 
 out = cv2.drawMatches(img1, kp1, img2, kp2, numMatches, None)
 cv2.imshow('Image', out)
