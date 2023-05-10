0543b39d825b6daf23d99df728db86b9835b34c5
Serpent03 <dumbpilot03@gmail.com>
2023-05-10 10:24:16 +0530

    feat: feature extraction and matching

--- brute_force.py
+++ brute_force.py
@@ -6,8 +6,15 @@ img1 = readIm('./Images/2.jpg')
 img2 = readIm('./Images/3.jpg')
 
 kp1, des1, kp2, des2 = ORB_detector(img1, img2)
+1
+numMatches = bruteForceMatcher(des1, des2)[:20]
 
-cv2.imshow('Image', cv2.drawKeypoints(img1, kp1, img1))
+# print(numMatches[:15])
+
+out = cv2.drawMatches(img1, kp1, img2, kp2, numMatches, None)
+cv2.imshow('Image', out)
+
+# cv2.imshow('Image', cv2.drawKeypoints(img1, kp1, img1))
 # cv2.imshow('Image', img2)
 
 cv2.waitKey(0)
