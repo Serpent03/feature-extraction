df2cb2fea47d251284694dd389c2d96995da70b2
Serpent03 <dumbpilot03@gmail.com>
2023-05-11 11:59:20 +0530

    minor refactoring

--- brute_force.py
+++ brute_force.py
@@ -2,33 +2,33 @@ import cv2
 
 from utils import *
 
+# TODO: implement multiple image system
+
+
 camMtx = retCamMtx("./cameraMatrix.pkl")
 nCamMtx = retCamMtx("./newCameraMatrix.pkl")
 distCoeff = retDistCoeff("./dist.pkl")
 
 img1 = readIm('./Images/2.jpg')
-img1 = retUndistortedIm(img1, camMtx, nCamMtx, distCoeff)
+# img1 = retUndistortedIm(img1, camMtx, nCamMtx, distCoeff)
 img2 = readIm('./Images/3.jpg')
-img2 = retUndistortedIm(img2, camMtx, nCamMtx, distCoeff)
+# img2 = retUndistortedIm(img2, camMtx, nCamMtx, distCoeff)
 limiter = 20 # number of matches
 
 kp1, des1, kp2, des2 = ORB_detector(img1, img2)
+# kp1, des1, kp2, des2 = BRISK_detector(img1, img2)
 kpL1 = retKpList(kp1)
 kpL2 = retKpList(kp2)
-numMatches = bruteForceMatcher(des1, des2)[:limiter]
+numMatches = bruteForceMatcher(des1, des2)
 # numMatches = bruteForceMatcherkNN(des1, des2)
-# print(numMatches)
 
 essMtx, _ = retEssentialMat(kpL1, kpL2, camMtx, distCoeff)
 _, R, t, mask = retPoseRecovery(essMtx, kpL1, kpL2)
 pts_3d = retTriangulation(R, t, kpL1, kpL2, limiter)
-# print(f'{pts_3d}')
+print(f'{pts_3d}')
 
-# NOTE=> displaying the elements in 3D matplotlib render here
+display2D(img1, kp1, img2, kp2, numMatches)
 display3D(pts_3d)
-
-out = cv2.drawMatches(img1, kp1, img2, kp2, numMatches, None)
-cv2.imshow('Image', out)
 
 # cv2.imshow('Image', cv2.drawKeypoints(img1, kp1, img1))
 # cv2.imshow('Image', img2)
--- utils.py
+++ utils.py
@@ -30,10 +30,16 @@ def retDistCoeff(pathToDistCoeff):
         return np.array(pickle.load(f))
 
 def ORB_detector(im1, im2):
-    detect = cv2.ORB_create()
+    detect = cv2.ORB_create(nfeatures=250)
     kp1, des1 = detect.detectAndCompute(im1, None)
     kp2, des2 = detect.detectAndCompute(im2, None)
     # print(kp1[0].pt)
+    return (kp1, des1, kp2, des2)
+
+def BRISK_detector(im1, im2):
+    brisk = cv2.BRISK_create(thresh=100)
+    kp1, des1 = brisk.detectAndCompute(im1, None)
+    kp2, des2 = brisk.detectAndCompute(im2, None)
     return (kp1, des1, kp2, des2)
 
 def retKpList(kp):
@@ -62,6 +68,8 @@ def retEssentialMat(kpL1, kpL2, camMtx, dist):
     # kpL1 = np.array(kpL1) # convert to np.array just before operation
     # kpL2 = np.array(kpL2)
     # dist = np.array(dist)
+    # NOTE : we convert them to np.array because cv2 expects a 
+    # NOTE <cv::UMat> format 
     c =  cv2.findEssentialMat(kpL1, kpL2, camMtx, None, None, None, None)
     return c
 
@@ -73,14 +81,19 @@ def retTriangulation(_R, _t, kpL1, kpL2, limiter):
     pts_3d = []
     proj_matrix = np.hstack((_R, _t))
     # print(kpL2[0])
-    # BUG=> the points need to be iterated..
+    # FIXME: the points need to be iterated..  
+
     for i in range(limiter):
-        pts_4d = cv2.triangulatePoints(np.eye(3, 4), proj_matrix, kpL1[i], kpL2[i])
+        pts_4d = cv2.triangulatePoints(proj_matrix, proj_matrix, kpL1[i], kpL2[i])
         pts = cv2.convertPointsFromHomogeneous(pts_4d.T)
         # print(pts)
         pts_3d.append([pts[0][0][0], pts[0][0][1], pts[0][0][2]])
     return pts_3d
     # return -1
+
+def display2D(img1, kp1, img2, kp2, numMatches):
+    out = cv2.drawMatches(img1, kp1, img2, kp2, numMatches, None)
+    cv2.imshow('Image', out)
 
 def display3D(pointCloud):
     fig = plt.figure()
